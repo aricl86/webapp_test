9b387054fbe7e4c472e4ed2d14d977ae2202df2a
aricl86 <aricl86@gmail.com>
2023-11-01 21:30:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
-import os
+from flask import Flask, render_template
+import folium
+import geopandas as gpd
+from sqlalchemy import create_engine
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+app = Flask(__name)
 
-app = Flask(__name__)
-
+# Azure PostgreSQL connection string
+connection_string = "postgresql+psycopg2://AdminDB:Aa1234567Aa1234567@isilgeodbdev.postgres.database.azure.com:5432/postgres"
+engine = create_engine(connection_string, timeout=60)
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def map_view():
+    # Create a connection to the Azure PostgreSQL database using SQLAlchemy
+    engine = create_engine(connection_string)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    # Read the "polygons_table" as a GeoDataFrame
+    query = "SELECT * FROM polygons_data"
+    gdf = gpd.read_postgis(query, con=engine)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    # Create a map with Folium
+    m = folium.Map(location=[0, 0], zoom_start=2)
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Create a GeoJSON layer and add it to the map
+    feature_group = folium.FeatureGroup(name="Polygons")
 
+    for idx, row in gdf.iterrows():
+        # Access the geometry column
+        geometry = row['geometry']
+
+        # Create a GeoJSON layer for each polygon
+        polygon_layer = folium.GeoJson(data=geometry.__geo_interface__)
+        polygon_layer.add_to(feature_group)
+
+    feature_group.add_to(m)
+    folium.LayerControl().add_to(m)
+
+    # Save the map to an HTML file
+    m.save('templates/map.html')
+
+    return render_template('map.html')
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
