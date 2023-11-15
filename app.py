0ed8896fb3565357c79e0e23d3ae9ec297f654dd
aricl86 <aricl86@gmail.com>
2023-11-15 13:49:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import time
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello Azure!</h1>"
+    # Add a 5-minute delay
+    time.sleep(300)
+    return "<h1>Hello Azure! After 5 minutes</h1>"
+
+if __name__ == "__main__":
+    app.run()
