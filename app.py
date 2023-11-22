11b8c06594d37b43439a15b5d3ae8f5bea3d03ce
aricl86 <aricl86@gmail.com>
2023-11-22 14:27:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 # Azure PostgreSQL connection string
-connection_string = "postgresql+psycopg2://AdminDB:Aa1234567Aa1234567@isilgeodbdev.postgres.database.azure.com:5432/postgres"
+connection_string = "postgresql+psycopg2://AdminDB@isilgeodbdev:Aa1234567Aa1234567@isilgeodbdev.postgres.database.azure.com:5432/postgres"
 
 @app.route('/')
 def map_view():
@@ -14,18 +14,25 @@ def map_view():
     engine = create_engine(connection_string)
 
     # Read the "polygons_table" as a GeoDataFrame
-    query = "SELECT * FROM polygons_table"
+    query = "SELECT * FROM polygons_data"
     gdf = gpd.read_postgis(query, con=engine)
 
-    # Create a map with Folium
-    m = folium.Map(location=[0, 0], zoom_start=2)
+    # Get the bounds of all geometries in the GeoDataFrame
+    bounds = gdf.total_bounds
+
+    # Calculate the center of the bounding box
+    center = [(bounds[1] + bounds[3]) / 2, (bounds[0] + bounds[2]) / 2]
+
+    # Create a map with Folium centered on the bounding box
+    m = folium.Map(location=center, zoom_start=16)  # You can adjust the initial zoom level
+
 
     # Create a GeoJSON layer and add it to the map
     feature_group = folium.FeatureGroup(name="Polygons")
 
     for idx, row in gdf.iterrows():
         # Access the geometry column
-        geometry = row['geometry']
+        geometry = row['geom']
 
         # Create a GeoJSON layer for each polygon
         polygon_layer = folium.GeoJson(data=geometry.__geo_interface__)
